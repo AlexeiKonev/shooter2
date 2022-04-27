a9c4e709d95b00b59e0d3908a588db87fd5351ab
AlexeiKonev <alexkonev777@gmail.com>
2022-04-27 20:49:03 +0300

    исправил прицел меняет цвет

---
+++ Assets/Scripts/HoverCursor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+// ставим скрипт на врага чтобы при наведении менять цвет прицела
+
+public class HoverCursor : MonoBehaviour
+{
+    
+
+    private void OnMouseEnter()
+    {
+        LookCamera.ChangeColorRed();
+    }
+
+    private void OnMouseExit()
+    {
+        LookCamera.ChangeColorYelow();
+    }
+}
--- Assets/Scripts/LookCamera.cs
+++ Assets/Scripts/LookCamera.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LookCamera : MonoBehaviour
 {
     public float sensityvyHorizont = 9f;
+
+    //картинка прицела
+    
+    public static Image _cursor;
+    //red (255,0,0)     yellow (255,255,0)
     void Start()
     {
-        
+        _cursor = GameObject.Find("Scope").GetComponent<Image>();
+        ChangeColorYelow();
+        //фиксируем курсор по центру и делаем невидимым
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, Input.GetAxis("Mouse X") * sensityvyHorizont, 0);
+    }
+
+    public static void ChangeColorYelow() 
+    {
+        
+        //желтый
+        _cursor.color = new Color(255, 255, 0);
+    } 
+    public static void ChangeColorRed() 
+    {
+
+        //красный
+        _cursor.color = new Color(255, 0, 0);
     }
 }
